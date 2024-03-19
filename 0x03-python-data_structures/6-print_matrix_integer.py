ab0152f6b3add32f0e156fbb55382df424652d5c
Droidna405 <joemwangala@gmail.com>
2024-03-19 02:13:40 -0400

    printing a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for idx, element in enumerate (row):
+            if idx < len(row) - 1:
+                print(end="")
+            print("{}".format(element), end=" ")
+            
+        print()

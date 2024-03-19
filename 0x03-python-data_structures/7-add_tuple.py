d674107492e61c93b25a92924bc4fa17e9fff498
Droidna405 <joemwangala@gmail.com>
2024-03-19 07:54:33 -0400

    Adding 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,5 +6,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     sum_first_elements = padded_tuple_a[0] + padded_tuple_b[0]
     sum_second_elements = padded_tuple_a[1] + padded_tuple_b[1]
-    
+
     return (sum_first_elements, sum_second_elements)

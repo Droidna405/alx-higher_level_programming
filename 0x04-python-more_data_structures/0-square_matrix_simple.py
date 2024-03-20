88af8b3436de7013048016b923eb9c084b528cb9
Droidna405 <joemwangala@gmail.com>
2024-03-20 21:24:32 +0300

    square of integers in a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        return matrix
-    row_length = len(matrix[0])
-        for row in matrix:
-            if len(row) != row_length:
-                raise ValueError("Input matrix must be a 2D list with consistent row lengths")
-    squared_matrix = [[0 for _ in range(row_length)] for _ in range(len(matrix))]
-
-    for i, row in enumerate(matrix):
-        for j, element in enumerate(row):
-            squared_matrix[i][j] = element * element
-    return squared_matrix
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(num ** 2)
+        new_matrix.append(new_row)
+    return new_matrix

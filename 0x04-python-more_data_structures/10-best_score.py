eeec979179a7ef9d407cf680066cb2e18da72644
Droidna405 <joemwangala@gmail.com>
2024-03-20 22:16:25 +0300

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary:
-        max_score = max(a_dictionary.values())
-        for key, value in a_dictionary.items():
-            if value == max_score:
-                return key
-            return None
+    if not a_dictionary:
+        return None
+    best_key = None
+    best_score = float("-inf")
+    for key, value in a_dictionary.items():
+        if not isinstance(value, int):
+            raise TypeError("Dictionary can only contain integer values")
+        if value > best_score:
+            best_score = value
+            best_key = key
+    return best_key

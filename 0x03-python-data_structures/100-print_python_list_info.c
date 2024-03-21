48b36ddcb10bc4a14d9be36a5531f324adbb283a
Droidna405 <joemwangala@gmail.com>
2024-03-21 01:48:54 -0400

    Cpython

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,36 +1,17 @@
-#include <python.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <Python.h>
 
 /**
- * print_python_list_info - Prints basic info about a Python list
- *
- * @p: A pointer to the Python object
- *
- * Description:
- *   This function prints basic information about a Python list object.
- *
- *   - If the object is not a PyList object, it prints an error message
- *     and returns.
- *   - It prints the size of the list.
- *   - It attempts to retrieve the first element of the list and, if
- *     successful, it prints the type of the first element.
- *
- *   It properly decrements the reference counts of any borrowed Python
- *   objects to avoid memory leaks.
- *
- * Return:
- *   Nothing (void)
+ * print_python_list_info - Prints basic information about a Python list
+ * @p: Pointer to PyObject (assumed to be a list)
  */
 void print_python_list_info(PyObject *p)
 {
-PyListObject *list = (PyListObject *)p;
-Py_ssize_t size = PyList_Size(p);
-Py_ssize_t i;
+Py_ssize_t size, i;
 PyObject *item;
 
+size = PyList_Size(p);
 printf("[*] Size of the Python List = %ld\n", size);
-printf("[*] Allocated = %ld\n", list->allocated);
+printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
 for (i = 0; i < size; i++)
 {

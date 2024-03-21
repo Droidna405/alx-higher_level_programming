890da249981ee0a4290e7eb4dde8ebaccadb3874
Droidna405 <joemwangala@gmail.com>
2024-03-21 02:16:28 -0400

    Linked list Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,71 +4,65 @@
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to pointer to the head of the list
- * Return: 1 if palindrome, 0 if not
+ * @head: a double pointer to the head of the linked list
+ *
+ * Return: If the list is a palindrome - 1.
+ *         Otherwise - 0.
  */
 int is_palindrome(listint_t **head)
 {
 listint_t *slow = *head, *fast = *head;
-listint_t *prev_slow = NULL;
-listint_t *second_half, *mid_node = NULL;
-int is_palindrome = 1;
+listint_t *prev_slow = NULL, *second_half = NULL;
 
-if (*head == NULL || (*head)->next == NULL)
-return (1);
-
+/* Get the middle of the list using two pointers */
 while (fast != NULL && fast->next != NULL)
 {
 fast = fast->next->next;
-
 prev_slow = slow;
 slow = slow->next;
 }
 
+/* If fast is not NULL, the list has odd length, so skip the middle node */
 if (fast != NULL)
-{
-mid_node = slow;
 slow = slow->next;
-}
 
+/* Reverse the second half of the list */
 second_half = slow;
-prev_slow->next = NULL;
+prev_slow->next = NULL;  // Mark the end of first half
 
-listint_t *prev = NULL, *current = second_half, *next;
-while (current != NULL)
-{
-next = current->next;
-current->next = prev;
-prev = current;
-current = next;
-}
-second_half = prev;
-
-listint_t *first_half = *head;
 while (second_half != NULL)
 {
-if (first_half->n != second_half->n)
+listint_t *next = second_half->next;
+second_half->next = *head;
+*head = second_half;
+second_half = next;
+}
+
+/* Compare first and second half for palindrome */
+slow = *head;
+fast = second_half;
+int is_palin = 1;
+while (slow != NULL && fast != NULL)
 {
-is_palindrome = 0;
+if (slow->n != fast->n)
+{
+is_palin = 0;
 break;
 }
-first_half = first_half->next;
-second_half = second_half->next;
+slow = slow->next;
+fast = fast->next;
 }
 
-prev = NULL;
-current = mid_node;
-while (current != NULL)
+/* Restore the original list */
+second_half = *head;
+*head = slow;
+while (second_half != NULL)
 {
-next = current->next;
-current->next = prev;
-prev = current;
-current = next;
+listint_t *next = second_half->next;
+second_half->next = *head;
+*head = second_half;
+second_half = next;
 }
-if (prev_slow != NULL)
-prev_slow->next = prev;
-else
-*head = prev;
 
-return (is_palindrome);
+return (is_palin);
 }

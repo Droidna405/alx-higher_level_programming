ab4bdd2e2e03562f73b3faab1574457001ed8eeb
Droidna405 <joemwangala@gmail.com>
2024-04-10 11:21:01 +0300

    Defining a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This module defines the Rectangle class representing a geometric rectangle.
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class represents a geometric rectangle.
+
+    Attributes:
+        No attributes yet.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new Rectangle object.
+
+        Args:
+            No arguments needed.
+        """
+        pass

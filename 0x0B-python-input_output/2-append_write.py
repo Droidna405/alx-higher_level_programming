4bc0d3de59af411fb3c0e058b6eab24c8334a260
Droidna405 <joemwangala@gmail.com>
2024-04-16 09:10:30 -0400

    Appends text to a txt file and creates one if it doesn't exit

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Appends a string at the end of a txt file (UTF8)"""
+def append_write(filename="", text=""):
+    """
+    Args:
+        filename (strng): The name of the text file.
+        text (strng): The string to append to the file.
+    Returns:
+        int: The number of characters added to the file.
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        file.write(text)
+        return len(text)

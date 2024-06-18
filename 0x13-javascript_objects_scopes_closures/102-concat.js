779ae630f66b8ff4ad9919c167a895668452a808
Droidna405 <joemwangala@gmail.com>
2024-06-18 21:24:19 +0300

    Concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+if (process.argv.length !== 5) {
+  console.error('Usage: ./102-concat.js fileA fileB fileC');
+  process.exit(1);
+}
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+try {
+  // Read contents of fileA and fileB
+  const contentA = fs.readFileSync(fileA, 'utf8');
+  const contentB = fs.readFileSync(fileB, 'utf8');
+
+  // Concatenate contents
+  const concatenatedContent = contentA + contentB;
+
+  // Write concatenated content to fileC
+  fs.writeFileSync(fileC, concatenatedContent);
+
+  console.log(`Concatenation of ${fileA} and ${fileB} completed. Result written to ${fileC}`);
+} catch (err) {
+  console.error(`Error: ${err.message}`);
+  process.exit(1);
+}

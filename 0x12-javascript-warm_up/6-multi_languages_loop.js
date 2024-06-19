753be67666a9eb5b5604ba3df65c4fe097c367cd
Droidna405 <joemwangala@gmail.com>
2024-06-19 20:00:49 +0300

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const messages = [
-  "C is fun",
-  "Python is cool",
-  "JavaScript is amazing"
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 for (let i = 0; i < messages.length; i++) {

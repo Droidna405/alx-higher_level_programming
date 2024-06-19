9b1809eac1aaa38572aaa107c6630a501d823ae8
Droidna405 <joemwangala@gmail.com>
2024-06-19 09:28:10 +0300

    Concat strings in JavaScript

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,8 +2,7 @@
 
 const myArguments = process.argv.slice(2);
 if (!myArguments[0]) {
-    console.log("undefined is undefined");
-}
-else {
-    console.log(myArguments[0], 'is', myArguments[1]);
+  console.log('undefined is undefined');
+} else {
+  console.log(myArguments[0], 'is', myArguments[1]);
 }

b5eba9930aef499f213e5ab128f51ee70121125a
Droidna405 <joemwangala@gmail.com>
2024-07-06 22:15:07 +0300

    First Commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,19 @@ using the urllib package and displays the response.
 
 
 import urllib.request
-with urllib.request.urlopen('https://intranet.htbn.io/status') as response:
-    data = response.read()
-    print("Body response:")
-    print(f"\t- type: {type(data)}")
-    print(f"\t- content: {data}")
-    print(f"\t- utf8 content: {data.decode('utf-8')}")
+
+
+def fetch status():
+    """
+    Fetches the status from a URL and prints the response details.
+    """
+    with urllib.request.urlopen('https://intranet.htbn.io/status') as response:
+        data = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(data)}")
+        print(f"\t- content: {data}")
+        print(f"\t- utf8 content: {data.decode('utf-8')}")
+
+
+if __name__ == "__main__":
+    fetch_status()
